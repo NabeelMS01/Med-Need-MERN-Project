40ef479903331f687f7900aeb2a5a276e92b91b5
Nabeel M.S <85822073+NabeelMS01@users.noreply.github.com>
2022-08-19 12:07:07 +0530

    final comimit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv =require('dotenv')
+
+const app= express();
+dotenv.config();
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("running");
+})
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,console.log(`server started at PORT : ${PORT}`))
